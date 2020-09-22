f65cd14d5f5d82b7d766d012fa9bb5626e66b57c
ferhatgec <ferhatgectao@gmail.com>
2020-09-22 21:54:38 +0300

    Add test1

---
+++ examples/test1/make.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+g++ ../../src/FString.cpp test1.cpp -o test
---
+++ examples/test1/test1.cpp
@@ -0,0 +1,17 @@
+/* MIT License
+#
+# Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
+# Distributed under the terms of the MIT License.
+#
+# */
+
+#include <iostream>
+#include "../../include/FString.hpp"
+
+int main(void) {
+        FString a('a');
+
+        std::cout << "A: " << a << "\n";
+
+        return 0;
+}
