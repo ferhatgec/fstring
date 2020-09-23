175a09a1fc1dd7e59b6c93f0fb3a3312e197c614
ferhatgec <ferhatgectao@gmail.com>
2020-09-23 17:02:31 +0300

    Add test9

---
+++ examples/test9/test9.cpp
@@ -0,0 +1,19 @@
+/* MIT License
+#
+# Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
+# Distributed under the terms of the MIT License.
+#
+# */
+
+#include <iostream>
+
+#include "../../include/FString.hpp"
+#include "../../include/Extend.hpp"
+
+int main(void) {
+        FString a("Hello, world!\n");
+
+        FExtend::Print(a);
+
+        return 0;
+}
