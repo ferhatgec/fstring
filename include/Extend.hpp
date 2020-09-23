6e6b129b85a0dc8ce0e83cc25cd97d50ad64ce4b
ferhatgec <ferhatgectao@gmail.com>
2020-09-23 17:03:02 +0300

    Add Extend.hpp/.cpp

---
+++ include/Extend.hpp
@@ -0,0 +1,19 @@
+/* MIT License
+#
+# Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
+# Distributed under the terms of the MIT License.
+#
+# */
+
+#ifndef EXTEND_HPP
+#define EXTEND_HPP
+
+#include <iostream>
+#include "FString.hpp"
+
+namespace FExtend {
+        int Print(const FString& _str, ...);
+}
+
+
+#endif /* EXTEND_HPP */
---
+++ src/Extend.cpp
@@ -0,0 +1,30 @@
+/* MIT License
+#
+# Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
+# Distributed under the terms of the MIT License.
+#
+# */
+
+#include <iostream>
+#include <cstdio>
+#include <cstdarg>
+#include "../include/Extend.hpp"
+#include "../include/FString.hpp"
+
+int FExtend::Print(const FString& _str, ...) {
+        /* Create new char* with _str.len */
+        char* data = new char[_str.len()];
+        va_list arg;
+  	int done;
+
+        /* Copy data */  
+        for(unsigned f = 0; f < _str.len(); f++)
+                data[f] = _str[f];
+
+  	va_start (arg, NULL);
+        done = vfprintf (stdout, data, arg);
+  	
+        va_end (arg);
+
+        return done;
+}
