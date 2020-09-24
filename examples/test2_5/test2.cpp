7e3dc07291c8374794c5126f84da1066b191bd83
ferhatgec <ferhatgectao@gmail.com>
2020-09-25 00:21:26 +0300

    Add test2_5

---
+++ examples/test2_5/test2.cpp
@@ -0,0 +1,22 @@
+/* MIT License
+#
+# Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
+# Distributed under the terms of the MIT License.
+#
+# */
+
+#include <iostream>
+#include "../../include/FString.hpp"
+
+int main(void) {
+        FString a("Hey!");
+        FString b(" Huh..");
+
+        std::cout << "A: " << a << "\n";
+        
+        a.append(b, " Today looks like milky filter coffee...");
+
+        std::cout << "New A: " << a << "\n";
+
+        return 0;
+}
