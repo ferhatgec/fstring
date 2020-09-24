ee7f8ff755572e2ecf14a992d2d53e08fdf98c6c
ferhatgec <ferhatgectao@gmail.com>
2020-09-24 19:42:24 +0300

    Add test2_3

---
+++ examples/test2_3/test2.cpp
@@ -0,0 +1,22 @@
+/* MIT License
+#
+# Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
+# Distributed under the terms of the MIT License.
+#
+# */
+
+#include <iostream>
+#include "../../include/FString.hpp"
+
+int main(void) {
+        FString a('a');
+
+        std::cout << "A: " << a << "\n";
+
+        /* a = atheist */
+        a.append("theist");
+        
+        std::cout << "New A: " << a << "\n";
+
+        return 0;
+}
