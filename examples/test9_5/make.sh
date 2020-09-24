4824214ee2da49bb117044c6d97bbe8f6b25cb3b
ferhatgec <ferhatgectao@gmail.com>
2020-09-24 17:20:26 +0300

    Add test9_5

---
+++ examples/test9_5/make.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+g++ ../../src/FString.cpp ../../src/Extend.cpp test9.cpp -o test
---
+++ examples/test9_5/test9.cpp
@@ -0,0 +1,36 @@
+/* MIT License
+#
+# Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
+# Distributed under the terms of the MIT License.
+#
+# */
+
+#include <iostream>
+
+#include "../../include/FString.hpp"
+#include "../../include/Extend.hpp"
+
+int main(void) {
+        FString a("Hello, world!\n");
+        FString b("This is FString\n");
+
+        std::cout << "A: ";
+        FExtend::Print(a);
+
+        std::cout << "A (Length): " << a.len();
+
+        a.equal('@');
+
+        std::cout << "\n-------------\n";
+
+        std::cout << "A (New): ";
+        FExtend::Print(a);
+
+        std::cout << "\nA (New length): " << a.len();
+
+
+
+        FExtend::Print("\n");
+        
+        return 0;
+}
