8f2ca4ad8a2eb02ce191dc397cb0e8b7b590ec7b
ferhatgec <ferhatgectao@gmail.com>
2020-10-13 18:53:47 +0300

    Add operator>>

--- include/FString.hpp
+++ include/FString.hpp
@@ -33,6 +33,9 @@ public:
     friend std::ostream& operator<< (std::ostream& ostr, 
         const FString& _str);
         
+    friend std::istream& operator<< (std::istream& istr, 
+    	FString& str);    
+        
     char operator[] (unsigned _ch) const;
     char& operator[] (unsigned _ch);
 
